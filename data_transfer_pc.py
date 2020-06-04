7a36076ba2f6baa13323bb8e55bd9b6ca5902778
SHiziw <45631851+SHiziw@users.noreply.github.com>
2020-06-04 16:45:46 +0800

    add transfering between pc and Rpi.

--- data_transfer_pc.py
+++ data_transfer_pc.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python3
 
-# File name   : PC.py
-
-# Author  : William
-
-# Date: 2019/12/23
-
-
-
 import cv2
-
 import zmq
-
 import base64
-
 import numpy as np
 
 
 
 context = zmq.Context()
-
 footage_socket = context.socket(zmq.PAIR)
-
 footage_socket.bind('tcp://*:5555')
 
 
 
 while True:
-
-frame = footage_socket.recv_string()
-
-img = base64.b64decode(frame)
-
-npimg = np.frombuffer(img, dtype=np.uint8)
-
-source = cv2.imdecode(npimg, 1)
-
-cv2.imshow("Stream", source)
-
-cv2.waitKey(1)
+    frame = footage_socket.recv_string()
+    img = base64.b64decode(frame)
+    npimg = np.frombuffer(img, dtype=np.uint8)
+    source = cv2.imdecode(npimg, 1)
+    cv2.imshow("Stream", source)
+    cv2.waitKey(1)

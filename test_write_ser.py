2013a155f7352c84253e2212343fdf3e032097f3
SHiziw <45631851+SHiziw@users.noreply.github.com>
2020-12-17 11:19:16 +0800

    testing serial-LF communication-3.

--- test_write_ser.py
+++ test_write_ser.py
@@ -7,10 +7,12 @@ ser = serial.Serial("/dev/ttyAMA0", 9600)
 
 def main():
     while True:
+        print("type in your command：",end="")
         send_thing = input()
         if send_thing != None:
-            print("sending: {0}".format(send_thing))
-            ser.write(send_thing)
+            print("sending: {0} ...".format(send_thing))
+            ser.write(send_thing.encode("UTF-8"))
+            print("OK!")
         time.sleep(0.1)
    
 if __name__ == "__main__":

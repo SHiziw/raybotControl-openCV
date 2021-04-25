bfadb6cdf378cc698dfe01e1c4554d9074d3ab4d
SHiziw <45631851+SHiziw@users.noreply.github.com>
2021-04-25 17:17:20 +0800

    完成岸基设备界面设计

--- client_tkinter_SBE.py
+++ client_tkinter_SBE.py
@@ -28,23 +28,54 @@
 # ==============================================================================
 debug = True
 
+from struct import pack,unpack #用于编码浮点数为字节流
 import tkinter as tk
 if not debug:
     import serial
     ser = serial.Serial("/dev/ttyAMA0", 9600)
 
+fl = 2.0
+fr = 2.0
+fu = 2100.0
+fd =900.0
+para_list = {"ML":fr,"MR":fl,"MU":fu,"MD":fd}
+
 def hit_me():
+    for key in para_list.keys():
+        sender(key,para_list[key])
+        print_selection("全部命令已发送")
+
+def sender(header,var):
+    byte=header.encode("UTF-8") +pack('f',var)
     if not debug:    
-        ser.write("MF000F000".encode("UTF-8"))
-def method(event):
-    s = '右侧的取值为' + str(right_speed.get())
-    global r_command
-    #print("---------------------------")
-    r_command = str(right_speed.get())
-   # print(command)
-   # print("---------------------------")
+        ser.write(byte)
+        ser.write(byte)
+    return byte
 
+def sync_method(header,var):
+    para_list[header] = float(var.get())
+    #print_selection("---------------------------")
+    print_selection("表头：{0}，更新为：{1}".format(header,para_list[header]))
+    # print_selection("---------------------------")
 
+def trun_method(header,var):
+    sender("ML",0.0)
+    sender("MR",0.0)
+    command = sender(header,var)
+    #print_selection("---------------------------")
+    print_selection("主动鳍：{0}，频率：{1}".format(header,var))
+    # print_selection("---------------------------")
+
+def forward_start_method(event):
+    sender("ML",para_list["ML"])
+    sender("MR",para_list["MR"])
+    print_selection("左频率：{0}，右频率：{1}".format(para_list["ML"],para_list["MR"]))
+def end_method(event):
+    sender("MR",0.0)
+    sender("ML",0.0)
+    print_selection(" ")
+def print_selection(v):
+    l.config(text=v)
 
 root= tk.Tk()
 root.title('RB3tcp控制端')
@@ -53,25 +84,75 @@ if not debug:
     root.attributes('-fullscreen', True)
 
 
-botton_frame = tk.Frame(root)
-botton_frame.pack()
-left_frame = tk.Frame(root)
-left_frame.pack(side="left",fill='both',expand='yes')
+left_frame = tk.Frame(root,height=320, width=240)
+left_frame.place(relx=0.25, rely=0.5, relheight=0.95, relwidth=0.5, anchor='center')
 right_frame = tk.Frame(root)
-right_frame.pack(side="right",fill='both',expand='yes')
+right_frame.place(relx=0.75, rely=0.5, relheight=0.95, relwidth=0.5, anchor='center')
 
-botton_estabilish = tk.Button(left_frame, text='退出系统', font=('黑体', 6), width=10, height=2, command=root.quit)
-botton_estabilish.pack()
- 
-botton_send = tk.Button(right_frame, text='发送命令', font=('黑体', 6), width=10, height=2, command=hit_me)
-botton_send.pack()
+#----左边-----
+#布局
+botton_frame = tk.Frame(left_frame)
+botton_frame.pack(side="top")
+arrow_frame = tk.Frame(left_frame)
+arrow_frame.pack(side="bottom",fill='both',expand='yes')
+#顶部按钮
+botton_estabilish = tk.Button(botton_frame, text='退出系统', font=('黑体', 6), width=10, height=2, command=root.quit)
+botton_estabilish.pack(side="left")
+botton_send = tk.Button(botton_frame, text='发送命令', font=('黑体', 6), width=10, height=2, command=hit_me)
+botton_send.pack(side="right")
+#方向键
+forward_key = tk.Button(arrow_frame, text='前进', font=('黑体', 6), width=10, height=5)
+forward_key.bind('<Button-1>',forward_start_method)
+forward_key.bind('<ButtonRelease-1>',end_method)
+forward_key.place(relx=0.5, rely=0.2, anchor='center')
+
+left_key = tk.Button(arrow_frame, text='左转', font=('黑体', 6), width=10, height=5)
+left_key.bind('<Button-1>',lambda event:trun_method("MR",para_list["ML"]))
+left_key.bind('<ButtonRelease-1>',end_method)
+left_key.place(relx=0.3, rely=0.45, anchor='center')
+
+right_key = tk.Button(arrow_frame, text='右转', font=('黑体', 6), width=10, height=5)
+right_key.bind('<Button-1>',lambda event:trun_method("ML",para_list["MR"]))
+right_key.bind('<ButtonRelease-1>',end_method)
+right_key.place(relx=0.7, rely=0.45, anchor='center')
+
+l = tk.Label(left_frame, bg='yellow', width=20, text=' ',height = 2,wraplength=100)
+l.pack(side="top")
+
+#----左边-----
+
+#----右边-----
+freq_left=tk.DoubleVar() #定义变量
+freq_right=tk.DoubleVar()
+fin_up=tk.DoubleVar()
+fin_down=tk.DoubleVar()
+freq_left.set(fl) #设定初始值
+freq_right.set(fr) 
+fin_up.set(fu)
+fin_down.set(fd)
 
 
-right_speed=tk.DoubleVar()
-right_slider = tk.Scale(right_frame,orient=tk.HORIZONTAL,from_=-100,to=100,font=('黑体',6),resolution=1,variable=right_speed)
-right_slider.bind('<ButtonRelease-1>',method)
-right_slider.pack()
+tk.Label(right_frame, text='左频率').grid(row=0, column=0, padx=10, pady=5, sticky='w')
+tk.Label(right_frame, text='右频率').grid(row=1, column=0, padx=10, pady=5, sticky='w')
+tk.Label(right_frame, text='拍动上限').grid(row=2, column=0, padx=10, pady=5, sticky='w')
+tk.Label(right_frame, text='拍动下限').grid(row=3, column=0, padx=10, pady=5, sticky='w')
 
+slider1 = tk.Scale(right_frame,orient=tk.HORIZONTAL,from_=0,to=10,font=('黑体',6),resolution=0.01,length=150,variable=freq_left)
+slider1.bind('<ButtonRelease-1>',lambda event:sync_method("ML",freq_left))
+slider1.grid(row=0, column=1, padx=10, pady=5)
+
+slider2 = tk.Scale(right_frame,orient=tk.HORIZONTAL,from_=0,to=10,font=('黑体',6),resolution=0.01,length=150,variable=freq_right)
+slider2.bind('<ButtonRelease-1>',lambda event:sync_method("MR",freq_right))
+slider2.grid(row=1, column=1, padx=10, pady=5)
+
+slider3 = tk.Scale(right_frame,orient=tk.HORIZONTAL,from_=900,to=2100,font=('黑体',6),resolution=0.01,length=150,variable=fin_up)
+slider3.bind('<ButtonRelease-1>',lambda event:sync_method("MU",fin_up))
+slider3.grid(row=2, column=1, padx=10, pady=5)
+
+slider4 = tk.Scale(right_frame,orient=tk.HORIZONTAL,from_=900,to=2100,font=('黑体',6),resolution=0.01,length=150,variable=fin_down)
+slider4.bind('<ButtonRelease-1>',lambda event:sync_method("MD",fin_down))
+slider4.grid(row=3, column=1, padx=10, pady=5)
+#----右边-----
 
 
 
